78677622ab5d7b2b6469b65772f816589c8a5f29
Shahzaib Iqbal <ishahzaib07@gmail.com>
2023-12-30 01:34:02 -0800

    Organize import statments

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { Table } from "@radix-ui/themes";
-import Link from "../components/Link";
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import { Table } from "@radix-ui/themes";
+import { Link, IssueStatusBadge } from "@/app/components";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
