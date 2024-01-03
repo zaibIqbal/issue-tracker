7d1c1ec6be1d5d426e2cebddfa9fc4f9bfc9417f
Shahzaib Iqbal <ishahzaib07@gmail.com>
2024-01-03 07:16:29 -0800

    Layout the dastboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { Flex, Grid } from "@radix-ui/themes";
 import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import prisma from "@/prisma/client";
+import LatestIssues from "./LatestIssues";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -9,5 +11,13 @@ export default async function Home() {
     where: { status: "IN_PROGRESS" },
   });
 
-  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+        <IssueChart open={open} closed={closed} inProgress={inProgress} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
